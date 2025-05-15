86008e05c912cd411581235803b08d3e1ffae2d7
Shohzod160 <shoxzadatajanov@gmail.com>
2025-05-15 13:41:16 +0500

    Delete audiobook-website-main

--- audiobook-website-main/api/signout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-
-session_unset(); // Удаляет все переменные сессии
-session_destroy(); // Уничтожает сессию
-
-echo json_encode(['success' => true, 'message' => 'Вы вышли из системы.']);
-?>
